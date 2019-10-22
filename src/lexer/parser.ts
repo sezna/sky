c8b01fed10b9ed5e7c8bd69b88fe21211f24781e
alex-hansen <ahansen@stackline.com>
2019-10-22 12:11:34 -0700

    refactor: move function declaration parsing to its own file
    In addition to moving function declaration parsing into its own file, I also fixed an infinite loop
    that was triggered on a parsing failure.

--- src/lexer/parser.ts
+++ src/lexer/parser.ts
@@ -1,16 +1,9 @@
 import { Either, right, left, isRight } from 'fp-ts/lib/Either';
 import { Tokens, Token } from './tokenizer';
+import { FunctionDeclaration, functionDeclaration } from './function-declaration';
 type Expression = IfExp | VarExp | OpExp | Literal;
 
 type Declaration = FunctionDeclaration | VariableDeclaration;
-
-interface FunctionDeclaration {
-    functionName: Token;
-    args: [Token, Token][];
-    body: Token[];
-    returnType: Token; // type-name
-}
-
 type Step = Expression | Declaration;
 type Steps = Step[];
 
@@ -40,7 +33,7 @@ interface Literal {
     literalValue: string;
 }
 
-interface ParseError {
+export interface ParseError {
     line: number;
     column: number;
     reason: string;
@@ -51,161 +44,19 @@ type Operator = '+' | '-'; // TODO
 // TODO return option of compileError or SyntaxTree -- have compileError bubble up
 export function makeSyntaxTree(input: Tokens): Either<ParseError, Steps> {
     let steps: Steps = [];
+                console.log("parsing succeeded, old input len is: ", input.length)
     while (input.length > 0) {
         if (input[0].tokenType === 'function-declaration') {
             const parseResult = functionDeclaration(input);
             if (isRight(parseResult)) {
                 // Then it wasn't an error.
                 input = parseResult.right.input;
+                console.log("new is: ", input.length);
                 steps.push(parseResult.right.declaration);
             } else {
-                left(parseResult.left);
+                return left(parseResult.left);
             }
         }
     }
     return right(steps);
-}
-
-function functionDeclaration(input: Tokens): Either<ParseError, { input: Tokens; declaration: FunctionDeclaration }> {
-    // Then we expect to see:
-    // function-decl, name, parens, name, ascription token, type-keyword, etc, parens, ascription token, type-keyword, curly-bracket
-    let token = input.shift()!; // 'fn'
-    const functionNameToken = input.shift()!;
-    const functionName = functionNameToken.value.value;
-    let prevToken = token;
-    token = input.shift()!;
-    if (token && token!.value.value !== '(') {
-        return left({
-            line: token.value.line,
-            column: token.value.column,
-            reason: `Malformed function declaration at line ${token.value.line}, column ${token.value.column}. Expected '(' but received '${token.value.value}.`,
-        });
-    }
-    if (token === undefined) {
-        return left({
-            line: prevToken.value.line,
-            column: prevToken.value.column,
-            reason: `Unexpected EOF after function keyword ('fn')`,
-        });
-    }
-    // Get the argument list out of the function signature
-    prevToken = token;
-    token = input.shift()!;
-    let args: [Token, Token][] = [];
-    while (token!.tokenType !== 'parens') {
-        if (token === undefined) {
-            return left({
-                line: prevToken.value.line,
-                column: prevToken.value.column,
-                reason: `Unexpected EOF in function "${functionName}" declaration after open parenthesis `,
-            });
-        }
-        const argName = token;
-        prevToken = token;
-        token = input.shift()!;
-        if (token && token.tokenType !== 'type-ascription') {
-            return left({
-                line: token.value.line,
-                column: token.value.column,
-                reason: `Malformed function declaration, missing type information in function "${functionName}". Expected ':' but received '${token.value.value}'. Example:
-                          fn ${functionName}(${argName.value.value}:<TYPE_IDENTIFIER>):<RETURN_TYPE_IDENTIFIER> { function body }`,
-            });
-        }
-        if (token === undefined) {
-            return left({
-                line: prevToken.value.line,
-                column: prevToken.value.column,
-                reason: `Unexpected EOF in function parameters declaration for function "${functionName}". Expected a colon (':') and type name after parameter "${argName.value.value}".`,
-            });
-        }
-        prevToken = token;
-        token = input.shift()!; // should be the type name
-        if (token && token.tokenType !== 'type-keyword') {
-            return left({
-                line: token.value.line,
-                column: token.value.column,
-                reason: `Invalid typename in function declaration for function "${functionName}". Expected a type name for parameter "${argName.value.value}" but received "${token.value.value}".`,
-            });
-        }
-        if (token === undefined) {
-            return left({
-                line: prevToken.value.line,
-                column: prevToken.value.column,
-                reason: `Unexpected EOF in function declaration for function "${functionName}" after colon (':'). Expected a type name for parameter "${argName.value.value}".`,
-            });
-        }
-        const typeName = token!;
-        args.push([argName, typeName]);
-
-        prevToken = token;
-        token = input.shift()!;
-    }
-    prevToken = token;
-    token = input.shift()!;
-    if (token && token.tokenType !== 'type-ascription') {
-        return left({
-            line: token.value.line,
-            column: token.value.column,
-            reason: `Malformed function declaration, missing type information in function "${functionName}" after the declaration for the parameters of function.`,
-        });
-    }
-    if (token === undefined) {
-        return left({
-            line: prevToken.value.line,
-            column: prevToken.value.column,
-            reason: `Unexpected EOF in function declaration for function "${functionName}" after colon (':'). Expected a type name.`,
-        });
-    }
-
-    prevToken = token;
-    token = input.shift()!;
-    if (token && token.tokenType !== 'type-keyword') {
-        return left({
-            line: token.value.line,
-            column: token.value.column,
-            reason: `Missing return type on function "${functionName}." Received "${token.value.value}" but expected a type name`,
-        });
-    }
-    const returnType = token!;
-    prevToken = token;
-    token = input.shift()!;
-    if (token!.value.value !== '{') {
-        return left({
-            line: token.value.line,
-            column: token.value.column,
-            reason: `Invalid function body in function "${functionName}." Received ${
-                token!.value.value
-            } but expected an opening curly bracket ('{').`,
-        });
-    }
-    if (token === undefined) {
-        return left({
-            line: prevToken.value.line,
-            column: prevToken.value.column,
-            reason: `Unexpected EOF in function declaration for function "${functionName}." Expected a body enclosed in curly brackets.`,
-        });
-    }
-    let body: Tokens = [];
-    while (token!.value.value !== '}') {
-        body.push(token!);
-        prevToken = token;
-        token = input.shift()!;
-        if (token === undefined) {
-            return left({
-                line: prevToken.value.line,
-                column: prevToken.value.column,
-                reason: `Missing closing curly bracket ('}') in function body for function "${functionName}".`,
-            });
-        }
-    }
-    console.log('parsed function declaration: ', functionName, args, body);
-    return right({
-        input,
-        declaration: {
-            functionName: functionNameToken,
-            args,
-            body,
-            returnType,
-        },
-    });
 }
