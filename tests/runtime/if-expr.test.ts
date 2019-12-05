b8dc61589936bb8349583da130f95c8fdeb17aae
Alex <alex@42layers.io>
2019-12-05 13:55:02 -0800

    TDD: tests for if expressions

---
+++ tests/runtime/if-expr.test.ts
@@ -0,0 +1,50 @@
+import { runtime } from '../../src/runtime';
+import { makeSyntaxTree } from '../../src/lexer/parser';
+import { tokenize } from '../../src/lexer/tokenizer';
+import { isLeft } from 'fp-ts/lib/Either';
+
+describe('if expr tests', () => {
+    it('should correctly assign its result to a variable #1', () => {
+        let program = `fn main(): song {
+                      number x = if 200 < 5 then 5 else 2;
+                   }`;
+        let steps = makeSyntaxTree(tokenize(program));
+
+        if (isLeft(steps)) {
+            console.log("Error: ", steps.left.reason);
+            // for typescript's type inference
+            expect(true).toBe(false);
+            return;
+        }
+        let result = runtime(steps.right);
+
+        if (isLeft(result)) {
+            console.log("Error: ", result.left.reason);
+            // for typescript's type inference
+            expect(true).toBe(false);
+            return;
+        }
+        expect(result.right.variableEnvironment['x'].value).toBe(2);
+    });
+    it('should correctly assign its result to a variable #2', () => {
+        let program = `fn main(): song {
+                      number x = if 200 > 5 then 5 else 2;
+                   }`;
+        let steps = makeSyntaxTree(tokenize(program));
+        if (isLeft(steps)) {
+            console.log("Error: ", steps.left.reason);
+            // for typescript's type inference
+            expect(true).toBe(false);
+            return;
+        }
+        let result = runtime(steps.right);
+
+        if (isLeft(result)) {
+            console.log("Error: ", result.left.reason);
+            // for typescript's type inference
+            expect(true).toBe(false);
+            return;
+        }
+        expect(result.right.variableEnvironment['x'].value).toBe(5);
+    });
+});
--- tests/typecheck.test.ts
+++ tests/typecheck.test.ts
@@ -67,3 +67,64 @@ describe('Invalid ops tests', () => {
         expect(isLeft(steps)).toBe(true);
     });
 });
+
+describe('If expression typechecking', () => {
+    it('Should not allow an if expression which returns two different types from its branches', () => {
+        let program = `fn main(): song { number x = if true then 5 else false; }`;
+
+        let steps = makeSyntaxTree(tokenize(program));
+        if (isRight(steps)) {
+            console.log('Steps are', JSON.stringify(steps, null, 2));
+            expect(true).toBe(false);
+            return;
+        }
+        expect(isLeft(steps)).toBe(true);
+        expect(steps.left.reason).toBe(
+            'Mismatched if expression: one branch returns type "number" while the other branch returns type "boolean"',
+        );
+    });
+    it('Should not allow an if expression which returns a different type than the declaration of the variable', () => {
+        let program = `fn main(): song { number x = if true then c#4 else d#4; }`;
+
+        let steps = makeSyntaxTree(tokenize(program));
+        if (isRight(steps)) {
+            console.log('Steps are', JSON.stringify(steps, null, 2));
+            expect(true).toBe(false);
+            return;
+        }
+        expect(isLeft(steps)).toBe(true);
+    });
+    it('Should allow an if expression result to be assigned to a variable', () => {
+        let program = `fn main(): song { number x = if 3 < 5 then 3 else 5; }`;
+
+        let steps = makeSyntaxTree(tokenize(program));
+        if (isLeft(steps)) {
+            console.log('Steps are', JSON.stringify(steps, null, 2));
+            expect(true).toBe(false);
+            return;
+        }
+        expect(isRight(steps)).toBe(true);
+    });
+    it('Should allow an if expression result to be not assigned to a variable', () => {
+        let program = `fn main(): song { if 3 < 5 then 3 else 5; }`;
+
+        let steps = makeSyntaxTree(tokenize(program));
+        if (isLeft(steps)) {
+            console.log('Steps are', JSON.stringify(steps, null, 2));
+            expect(true).toBe(false);
+            return;
+        }
+        expect(isRight(steps)).toBe(true);
+    });
+    it('Should not allow an if expression condition to be non-boolean', () => {
+        let program = `fn main(): song { if 5 then 3 else 5; }`;
+
+        let steps = makeSyntaxTree(tokenize(program));
+        if (isRight(steps)) {
+            console.log('Steps are', JSON.stringify(steps, null, 2));
+            expect(true).toBe(false);
+            return;
+        }
+        expect(isLeft(steps)).toBe(true);
+    });
+});
