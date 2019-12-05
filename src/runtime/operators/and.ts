181f105795ff30a731c5252c6ec239dc82a0187f
Alex <alex@42layers.io>
2019-12-05 13:32:42 -0800

    boolean operator implementations

---
+++ src/runtime/operators/and.ts
@@ -0,0 +1,14 @@
+import { EvalResult, RuntimeError } from '../';
+import { Either, right, left } from 'fp-ts/lib/Either';
+import { OperationSuccess } from './operation-success';
+// Handle the && operator
+export function and(lhs: EvalResult, rhs: EvalResult): Either<RuntimeError, OperationSuccess> {
+    if (lhs.returnType === 'boolean' && rhs.returnType === 'boolean') {
+        return right({ valueType: 'boolean', value: lhs.returnValue && rhs.returnValue });
+    }
+    return left({
+        line: 0,
+        column: 0,
+        reason: `Boolean and (&&) unimplemented for types "${lhs.returnType}" and "${rhs.returnType}"`,
+    });
+}
