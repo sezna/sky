c3ae8d279c5d63b56e2a356367eb416e3cb95310
Alex <alex@42layers.io>
2019-12-26 11:30:24 -0800

    git add forgotten file

---
+++ src/runtime/operators/equals.ts
@@ -0,0 +1,17 @@
+import { EvalResult, RuntimeError } from '../';
+import { Either, right, left } from 'fp-ts/lib/Either';
+import { OperationSuccess } from './operation-success';
+// Handle the == operator.
+export function equals(lhs: EvalResult, rhs: EvalResult): Either<RuntimeError, OperationSuccess> {
+    if (lhs.returnType !== rhs.returnType) {
+        return left({
+            line: 0,
+            column: 0,
+            reason: `Boolean equals (==) unimplemented for types "${lhs.returnType}" and "${rhs.returnType}"`,
+        });
+    }
+    return right({
+        valueType: 'boolean',
+        value: lhs.returnValue === rhs.returnValue,
+    });
+}
