40d8195b6f498af1b477b7518fb8fb68a5e19f30
Alex <alex@42layers.io>
2019-12-31 08:41:22 -0800

    more single note render tests

--- tests/renderer/single-note.test.ts
+++ tests/renderer/single-note.test.ts
@@ -23,4 +23,56 @@ describe('single note renderer tests', () => {
         let renderedAbc = render(runtimeResult.right);
         expect(renderedAbc.split('\n').pop()).toBe('c32');
     });
+    it('Should be able to render a sharp note, namely an a#', () => {
+        let program = `fn main(): pitch_rhythm { return a#6 whole; }`;
+        let stepsResult = makeSyntaxTree(tokenize(program));
+        if (isLeft(stepsResult)) {
+            console.log(`Parse error at line ${stepsResult.left.line}, column ${stepsResult.left.column}`);
+            expect(true).toBe(false);
+            return;
+        }
+        let runtimeResult = runtime(stepsResult.right);
+        if (isLeft(runtimeResult)) {
+            console.log(`Runtime error: ${runtimeResult.left.reason}`);
+            expect(true).toBe(false);
+            return;
+        }
+        let renderedAbc = render(runtimeResult.right);
+        expect(renderedAbc.split('\n').pop()).toBe("^a''128");
+    });
+    it('Should be able to render a flat note, namely a gb', () => {
+        let program = `fn main(): pitch_rhythm { return gb5 dotted half; }`;
+        let stepsResult = makeSyntaxTree(tokenize(program));
+        if (isLeft(stepsResult)) {
+            console.log(`Parse error at line ${stepsResult.left.line}, column ${stepsResult.left.column}`);
+            expect(true).toBe(false);
+            return;
+        }
+        let runtimeResult = runtime(stepsResult.right);
+        if (isLeft(runtimeResult)) {
+            console.log(`Runtime error: ${runtimeResult.left.reason}`);
+            expect(true).toBe(false);
+            return;
+        }
+        let renderedAbc = render(runtimeResult.right);
+        expect(renderedAbc.split('\n').pop()).toBe("_g'96");
+    });
+    it('Should be able to render a c#3 with a dotted quarter rhythm', () => {
+        let program = `fn main(): pitch_rhythm { return c#3 dotted quarter; }`;
+        let stepsResult = makeSyntaxTree(tokenize(program));
+        if (isLeft(stepsResult)) {
+            console.log(`Parse error at line ${stepsResult.left.line}, column ${stepsResult.left.column}`);
+            expect(true).toBe(false);
+            return;
+        }
+        let runtimeResult = runtime(stepsResult.right);
+        if (isLeft(runtimeResult)) {
+            console.log(`Runtime error: ${runtimeResult.left.reason}`);
+            expect(true).toBe(false);
+            return;
+        }
+
+        let renderedAbc = render(runtimeResult.right);
+        expect(renderedAbc.split('\n').pop()).toBe('^c,48');
+    });
 });
