eec6ef918e7b6e417553adc89b7af773bee0e604
Alex <alex@42layers.io>
2020-01-06 09:21:29 -0800

    TDD: More 2d list tests

--- tests/renderer/list-list.test.ts
+++ tests/renderer/list-list.test.ts
@@ -57,4 +57,96 @@ fn main(): list list pitch_rhythm {
         let renderedAbc = render(runtimeResult.right);
         expect(renderedAbc.split('\n').pop()).toBe('TODO');
     });
+    it('should be able to render a larger list of lists of notes (aka a song or piece)', () => {
+        let program = `
+fn main(): list list pitch_rhythm {
+				return [
+								[c4 quarter, c#4 quarter, d4 quarter, d#4 quarter],
+								[e4 quarter, d#4 quarter, d4 quarter, c#4 quarter]
+								[c3 quarter, e#3 quarter, f3 quarter, g#3 quarter],
+								[a1 quarter, c#3 quarter, d3 quarter, d#3 quarter],
+								[e3 quarter, d#3 quarter, d3 quarter, c#3 quarter]
+								[e4 quarter, d#4 quarter, d4 quarter, c#4 quarter]
+								];
+				}`;
+        let stepsResult = makeSyntaxTree(tokenize(program));
+        if (isLeft(stepsResult)) {
+            console.log(
+                `Parse error at line ${stepsResult.left.line}, column ${stepsResult.left.column}: ${stepsResult.left.reason}`,
+            );
+            expect(true).toBe(false);
+            return;
+        }
+        let runtimeResult = runtime(stepsResult.right);
+        if (isLeft(runtimeResult)) {
+            console.log(`Runtime error: ${runtimeResult.left.reason}`);
+            expect(true).toBe(false);
+            return;
+        }
+
+        let renderedAbc = render(runtimeResult.right);
+        expect(renderedAbc.split('\n').pop()).toBe('TODO');
+    });
+		// This next test brings up an interesting case - there should be two stdlib funcs, one that prepends rests until a certain 
+		// length and one that appends, for matching up parts in cases like this
+    it('should throw an error if one of the parts is too short', () => {
+        let program = `
+fn main(): list list pitch_rhythm {
+				return [
+								[c4 quarter, c#4 quarter, d4 quarter, d#4 quarter],
+								[e4 quarter, d#4 quarter, d4 quarter, c#4 quarter]
+								[c3 quarter, e#3 quarter, f3 quarter, g#3 quarter],
+								[a1 quarter, c#3 quarter, d3 quarter, d#3 quarter],
+								[e3 quarter, d#3 quarter, d3 quarter]
+								[e4 quarter, d#4 quarter, d4 quarter, c#4 quarter]
+								];
+				}`;
+        let stepsResult = makeSyntaxTree(tokenize(program));
+        if (isLeft(stepsResult)) {
+            console.log(
+                `Parse error at line ${stepsResult.left.line}, column ${stepsResult.left.column}: ${stepsResult.left.reason}`,
+            );
+            expect(true).toBe(false);
+            return;
+        }
+        let runtimeResult = runtime(stepsResult.right);
+        if (isLeft(runtimeResult)) {
+            console.log(`Runtime error: ${runtimeResult.left.reason}`);
+            expect(true).toBe(false);
+            return;
+        }
+
+        let renderedAbc = render(runtimeResult.right);
+        expect(renderedAbc.split('\n').pop()).toBe('TODO');
+    });
+    it('should throw an error if one of the parts is too long', () => {
+        let program = `
+fn main(): list list pitch_rhythm {
+				return [
+								[c4 quarter, c#4 quarter, d4 quarter, d#4 quarter],
+								[e4 quarter, d#4 quarter, d4 quarter, c#4 quarter]
+								[c3 quarter, e#3 quarter, f3 quarter, g#3 quarter],
+								[a1 quarter, c#3 quarter, d3 quarter, d#3 quarter],
+								[e3 quarter, d#3 quarter, d3 quarter]
+								[e4 quarter, d#4 quarter, d4 quarter, c#4 quarter]
+								];
+				}`;
+        let stepsResult = makeSyntaxTree(tokenize(program));
+        if (isLeft(stepsResult)) {
+            console.log(
+                `Parse error at line ${stepsResult.left.line}, column ${stepsResult.left.column}: ${stepsResult.left.reason}`,
+            );
+            expect(true).toBe(false);
+            return;
+        }
+        let runtimeResult = runtime(stepsResult.right);
+        if (isLeft(runtimeResult)) {
+            console.log(`Runtime error: ${runtimeResult.left.reason}`);
+            expect(true).toBe(false);
+            return;
+        }
+
+        let renderedAbc = render(runtimeResult.right);
+        expect(renderedAbc.split('\n').pop()).toBe('TODO');
+    });
 });
