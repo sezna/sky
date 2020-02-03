218670204d88f5327684a3985cd5c177cc558684
Alex <alex@42layers.io>
2020-02-03 15:59:16 -0800

    more properties tests

--- tests/runtime/assignments.test.ts
+++ tests/runtime/assignments.test.ts
@@ -1,7 +1,7 @@
 import { runtime } from '../../src/runtime';
 import { makeSyntaxTree } from '../../src/lexer/parser';
 import { tokenize } from '../../src/lexer/tokenizer';
-import { isLeft } from 'fp-ts/lib/Either';
+import { isLeft, isRight } from 'fp-ts/lib/Either';
 
 describe('assignment and reassignment tests', () => {
     it('be able to reassign a variable', () => {
@@ -56,5 +56,47 @@ describe('assignment and reassignment tests', () => {
         }
         expect(result.right.variableEnvironment['x'].properties).toHaveProperty('clef');
         expect(result.right.variableEnvironment['x'].properties.clef).toEqual('bass');
+    });
+    it('be able to assign a multi-word property', () => {
+        let program = `
+				fn main(): number {
+								number x = 0;
+								x.dynamic = mezzo forte;
+								return 0;
+				}
+				`;
+        let tokens = tokenize(program);
+        let steps = makeSyntaxTree(tokens);
+        if (isLeft(steps)) {
+            console.log('Steps are', JSON.stringify(steps, null, 2));
+            expect(true).toBe(false);
+            return;
+        }
+        let result = runtime(steps.right);
+        if (isLeft(result)) {
+            console.log(JSON.stringify(result, null, 2));
+            // for typescript's type inference
+            expect(true).toBe(false);
+            return;
+        }
+        expect(result.right.variableEnvironment['x'].properties).toHaveProperty('dynamic');
+        expect(result.right.variableEnvironment['x'].properties.dynamic).toEqual('mezzo forte');
+    });
+    it('should reject an invalid property', () => {
+        let program = `
+				fn main(): number {
+								number x = 0;
+								x.clef = something invalid;
+								return 0;
+				}
+				`;
+        let tokens = tokenize(program);
+        let steps = makeSyntaxTree(tokens);
+        if (isRight(steps)) {
+            expect(true).toBe(false);
+            return;
+        }
+        expect(isLeft(steps)).toBe(true);
+        expect(steps.left.reason).toBe('Value "something invalid" is not a valid property value.');
     });
 });
