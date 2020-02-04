a329b3b9215b8064859c21cf3888e4769405caba
Alex <alex@42layers.io>
2020-02-04 10:49:25 -0800

    add test; refactor a util

--- src/renderer/utils.ts
+++ src/renderer/utils.ts
@@ -1,6 +1,63 @@
 import { LiteralRhythm } from '../lexer/expression/literal';
 
 /**
+ * Generate header information pertaining to voices.
+ * Currently just defaults to treble clef with numbered voices.
+ * Support for other attributes is TODO.
+ * See this page for an idea of what needs to be done: http://abcnotation.com/wiki/abc:standard:v2.1#multiple_voices
+ */
+export function generateHeader(input: any): string {
+    const numberOfVoices = input.length;
+    let output = '';
+    for (let voiceIndex = 1; voiceIndex < numberOfVoices; voiceIndex++) {
+        output += `V:T${romanize(voiceIndex)} clef=treble-8 name="Voice ${voiceIndex}" snm="V.${voiceIndex}"\n`;
+    }
+    return output;
+}
+
+// https://stackoverflow.com/questions/9083037/convert-a-number-into-a-roman-numeral-in-javascript
+// all the libs on npm that do roman numeral conversion are pretty bad
+export function romanize(num: number) {
+    var digits = String(+num).split(''),
+        key = [
+            '',
+            'C',
+            'CC',
+            'CCC',
+            'CD',
+            'D',
+            'DC',
+            'DCC',
+            'DCCC',
+            'CM',
+            '',
+            'X',
+            'XX',
+            'XXX',
+            'XL',
+            'L',
+            'LX',
+            'LXX',
+            'LXXX',
+            'XC',
+            '',
+            'I',
+            'II',
+            'III',
+            'IV',
+            'V',
+            'VI',
+            'VII',
+            'VIII',
+            'IX',
+        ],
+        roman = '',
+        i = 3;
+    while (i--) roman = (key[+digits.pop()! + i * 10] || '') + roman;
+    return Array(+digits.join('') + 1).join('M') + roman;
+}
+
+/**
  * Convert the rhythm names into ABC note numbers.
  */
 export function convertDurationToAbc(input: LiteralRhythm): string {
--- tests/runtime/assignments.test.ts
+++ tests/runtime/assignments.test.ts
@@ -57,6 +57,32 @@ describe('assignment and reassignment tests', () => {
         expect(result.right.variableEnvironment['x'].properties).toHaveProperty('clef');
         expect(result.right.variableEnvironment['x'].properties.clef).toEqual('bass');
     });
+    it('be able to reassign a property', () => {
+        let program = `
+				fn main(): number {
+								number x = 0;
+								x.clef = bass;
+								x.clef = treble;
+								return 0;
+				}
+				`;
+        let tokens = tokenize(program);
+        let steps = makeSyntaxTree(tokens);
+        if (isLeft(steps)) {
+            console.log('Steps are', JSON.stringify(steps, null, 2));
+            expect(true).toBe(false);
+            return;
+        }
+        let result = runtime(steps.right);
+        if (isLeft(result)) {
+            console.log(JSON.stringify(result, null, 2));
+            // for typescript's type inference
+            expect(true).toBe(false);
+            return;
+        }
+        expect(result.right.variableEnvironment['x'].properties).toHaveProperty('clef');
+        expect(result.right.variableEnvironment['x'].properties.clef).toEqual('treble');
+    });
     it('be able to assign a multi-word property', () => {
         let program = `
 				fn main(): number {
@@ -82,6 +108,32 @@ describe('assignment and reassignment tests', () => {
         expect(result.right.variableEnvironment['x'].properties).toHaveProperty('dynamic');
         expect(result.right.variableEnvironment['x'].properties.dynamic).toEqual('mezzo forte');
     });
+    it('be able to reassign a multi-word property', () => {
+        let program = `
+				fn main(): number {
+								number x = 0;
+								x.dynamic = mezzo forte;
+								x.dynamic = forte;
+								return 0;
+				}
+				`;
+        let tokens = tokenize(program);
+        let steps = makeSyntaxTree(tokens);
+        if (isLeft(steps)) {
+            console.log('Steps are', JSON.stringify(steps, null, 2));
+            expect(true).toBe(false);
+            return;
+        }
+        let result = runtime(steps.right);
+        if (isLeft(result)) {
+            console.log(JSON.stringify(result, null, 2));
+            // for typescript's type inference
+            expect(true).toBe(false);
+            return;
+        }
+        expect(result.right.variableEnvironment['x'].properties).toHaveProperty('dynamic');
+        expect(result.right.variableEnvironment['x'].properties.dynamic).toEqual('forte');
+    });
     it('should reject an invalid property', () => {
         let program = `
 				fn main(): number {
@@ -98,5 +150,24 @@ describe('assignment and reassignment tests', () => {
         }
         expect(isLeft(steps)).toBe(true);
         expect(steps.left.reason).toBe('Value "something invalid" is not a valid property value.');
+    });
+    it('should reject a reassignment of the wrong type', () => {
+        let program = `
+				fn main(): number {
+								number x = 0;
+								x = c#3;
+								return x;
+				}
+				`;
+        let tokens = tokenize(program);
+        let steps = makeSyntaxTree(tokens);
+        if (isRight(steps)) {
+            expect(true).toBe(false);
+            return;
+        }
+        expect(isLeft(steps)).toBe(true);
+        expect(steps.left.reason).toBe(
+            'Attempted to assign value of type "pitch" to variable "x", which has type "number".',
+        );
     });
 });
