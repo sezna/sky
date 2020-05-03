84f303a7478c7ab1ed12064fc506f336c0dc7160
Alex <alex@42layers.io>
2020-05-03 10:15:26 -0700

    2d pitch-rhythm test

--- tests/xml-renderer/list-list-note.test.ts
+++ tests/xml-renderer/list-list-note.test.ts
@@ -355,4 +355,191 @@ fn main(): list list pitch {
     </part>
 </score-partwise>`);
     });
+it('List list pitch: sample program #1', () => {
+        let program = `fn main(): list list pitch_rhythm {
+  list list pitch_rhythm x = [
+          [c#4 quarter,  c#4 quarter,  d4 quarter,  e4 quarter],
+          [a4 quarter,  b4 quarter,  c4 quarter,  d4 quarter ],
+          [c#3 quarter, d3 half]
+         ];
+  x[0].part_id = part one;
+  x[1].part_id = yes this is a part id;
+  x[1][0] = g4 quarter;
+  x[2].clef = bass;
+  x.key = E major;
+  return x;
+}
+`;
+        let stepsResult = makeSyntaxTree(tokenize(program));
+        if (isLeft(stepsResult)) {
+            console.log(`Parse error at line ${stepsResult.left.line}, column ${stepsResult.left.column}`);
+            expect(true).toBe(false);
+            return;
+        }
+        let runtimeResult = runtime(stepsResult.right);
+        if (isLeft(runtimeResult)) {
+            console.log(`Runtime error: ${runtimeResult.left.reason}`);
+            expect(true).toBe(false);
+            return;
+        }
+
+        let renderedXml = render(runtimeResult.right);
+        expect(renderedXml).toBe(`<?xml version="1.0" encoding="UTF-8" standalone="no"?>
+<!DOCTYPE score-partwise PUBLIC
+    "-//Recordare//DTD MusicXML 3.0 Partwise//EN"
+    "http://www.musicxml.org/dtds/partwise.dtd">
+<score-partwise version="3.0">
+    <part-list>
+      <score-part id="part one">
+        <part-name>part one</part-name>
+      </score-part>
+      <score-part id="yes this is a part id">
+        <part-name>yes this is a part id</part-name>
+      </score-part>
+      <score-part id="P3">
+        <part-name>P3</part-name>
+      </score-part>
+    </part-list>
+    <part id="part one">
+        <measure number="1">
+            <attributes>
+                <divisions>144</divisions>
+                <key>
+                    <fifths>4</fifths>
+                    <mode>major</mode>
+                </key>
+                <time>
+                    <beats>4</beats>
+                    <beat-type>4</beat-type>
+                </time>
+                <clef>
+                    <sign>G</sign>
+                    <line>2</line>
+                </clef>
+            </attributes>
+            <note>
+                <pitch>
+                    <step>c</step>
+                    <octave>4</octave>
+                    <alter>1</alter>
+                </pitch>
+                <duration>144</duration>
+                <type>quarter</type>
+            </note>
+            <note>
+                <pitch>
+                    <step>c</step>
+                    <octave>4</octave>
+                    <alter>1</alter>
+                </pitch>
+                <duration>144</duration>
+                <type>quarter</type>
+            </note>
+            <note>
+                <pitch>
+                    <step>d</step>
+                    <octave>4</octave>
+                </pitch>
+                <duration>144</duration>
+                <type>quarter</type>
+            </note>
+            <note>
+                <pitch>
+                    <step>e</step>
+                    <octave>4</octave>
+                </pitch>
+                <duration>144</duration>
+                <type>quarter</type>
+            </note>
+        </measure>
+    </part>
+    <part id="yes this is a part id">
+        <measure number="1">
+            <attributes>
+                <divisions>144</divisions>
+                <key>
+                    <fifths>4</fifths>
+                    <mode>major</mode>
+                </key>
+                <time>
+                    <beats>4</beats>
+                    <beat-type>4</beat-type>
+                </time>
+                <clef>
+                    <sign>G</sign>
+                    <line>2</line>
+                </clef>
+            </attributes>
+            <note>
+                <pitch>
+                    <step>g</step>
+                    <octave>4</octave>
+                </pitch>
+                <duration>144</duration>
+                <type>quarter</type>
+            </note>
+            <note>
+                <pitch>
+                    <step>b</step>
+                    <octave>4</octave>
+                </pitch>
+                <duration>144</duration>
+                <type>quarter</type>
+            </note>
+            <note>
+                <pitch>
+                    <step>c</step>
+                    <octave>4</octave>
+                </pitch>
+                <duration>144</duration>
+                <type>quarter</type>
+            </note>
+            <note>
+                <pitch>
+                    <step>d</step>
+                    <octave>4</octave>
+                </pitch>
+                <duration>144</duration>
+                <type>quarter</type>
+            </note>
+        </measure>
+    </part>
+    <part id="P3">
+        <measure number="1">
+            <attributes>
+                <divisions>144</divisions>
+                <key>
+                    <fifths>4</fifths>
+                    <mode>major</mode>
+                </key>
+                <time>
+                    <beats>4</beats>
+                    <beat-type>4</beat-type>
+                </time>
+                <clef>
+                    <sign>F</sign>
+                    <line>4</line>
+                </clef>
+            </attributes>
+            <note>
+                <pitch>
+                    <step>c</step>
+                    <octave>3</octave>
+                    <alter>1</alter>
+                </pitch>
+                <duration>144</duration>
+                <type>quarter</type>
+            </note>
+            <note>
+                <pitch>
+                    <step>d</step>
+                    <octave>3</octave>
+                </pitch>
+                <duration>288</duration>
+                <type>half</type>
+            </note>
+        </measure>
+    </part>
+</score-partwise>`);
+    })
 });
